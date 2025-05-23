5624fb56559c455b3e42f48fbab159bbb2872b10
LeopoldoDelgadillo <leopoldo.d@encora.com>
2025-05-22 19:01:47 -0600

    modified:   frontend/src/app/globals.css 	modified:   frontend/src/app/page.tsx Added first block of frontend (search block, not functional yet)

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,15 +3,15 @@ export default function Home() {
     <main>
       <div 
         className="SearchBlock"
-        style={{border: "1px solid black"}}>
+        style={{border: "1px solid black", width: "450px", height: "135px", margin: "10px"}}>
         <form>
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name" name="name"></input><br></br>
-          <label htmlFor="categories">Category</label>
-          <input type="text" id="categories" name="categories"></input><br></br>
-          <label htmlFor="availability">Availability</label>
-          <input type="text" id="availability" name="availability"></input>
-          <input type="submit" value="Search"></input>
+          <label htmlFor="name" style={{margin: "10px"}}>Name</label>
+          <input type="text" id="name" name="name" style={{border: "1px solid black", margin: "10px"}}></input><br></br>
+          <label htmlFor="categories" style={{margin: "10px"}}>Category</label>
+          <select id="categories" name="categories" style={{border: "1px solid black", margin: "10px"}}></select><br></br>
+          <label htmlFor="availability" style={{margin: "10px"}}>Availability</label>
+          <select id="availability" name="availability" style={{border: "1px solid black", margin: "10px"}}></select>
+          <input type="submit" value="Search" style={{border: "1px solid black", margin: "10px"}}></input>
         </form>
       </div>
     </main>
